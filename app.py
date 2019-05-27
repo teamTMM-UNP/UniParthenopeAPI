2d9565fd0f0df7412f28efe71327109bf6472927
g.mell1 <shottyno90@gmail.com>
2019-05-28 00:34:29 +0200

    session

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, Blueprint, url_for, jsonify, current_app, abort
 from flask_restplus import Api, Resource, reqparse
+from datetime import date
 from flask_marshmallow import Marshmallow, base_fields
 from marshmallow import post_dump
 import werkzeug
 import requests
+import re
 import os
 
 app = Flask(__name__)
@@ -81,6 +83,29 @@ class TotalExams(Resource):
                             'base_trenta': base_trenta,
                             'base_centodieci': base_centodieci,
                             'centodieci': media_centodieci})
+
+
+@api.route('/api/uniparthenope/current_aa/<token>/<cdsId>', methods=['GET'])
+class CurrentAA(Resource):
+    def get(self, token, cdsId):
+        headers = {
+            'Content-Type': "application/json",
+            "Authorization": "Basic " + token
+        }
+        response = requests.request("GET", url + "calesa-service-v1/sessioni?cdsId=" + cdsId, headers=headers)
+        _response = response.json()
+
+        today = date.today()
+        max_year = 0
+        for i in range(0, len(_response)):
+            if _response[i]['aaSesId'] > max_year:
+                max_year = _response[i]['aaSesId']
+
+                dataInizio = re.search(r'\d{2}/\d{2}/\d{4}', _response[i]['dataInizio'])
+                dataFine = re.search(r'\d{2}/\d{2}/\d{4}', _response[i]['dataFine'])
+
+                print('D.INIZIO =' + dataInizio)
+                print('D.FINE =' + dataFine)
 
 
 if __name__ == '__main__':
