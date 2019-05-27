6d4333618bda74b8bfefefb4b7bfd13d8cc373cc
g.mell1 <shottyno90@gmail.com>
2019-05-27 22:51:07 +0200

    medie

--- app.py
+++ app.py
@@ -67,35 +67,12 @@ class TotalExams(Resource):
             "Authorization": "Basic " + token
         }
         if value != "P" or value != "A":
-            raise InvalidUsage('This view is gone', status_code=410)
+            return jsonify({'statusCode': 500,
+                            'retErrMsg': "Invalid value! (usage P or A)"})
 
         response = requests.request("GET", url + "libretto-service-v1/libretti/" + matId + "/medie", headers=headers)
 
         _response = response.json()
-
-
-class InvalidUsage(Exception):
-    status_code = 400
-
-    def __init__(self, message, status_code=None, payload=None):
-        Exception.__init__(self)
-        self.message = message
-        if status_code is not None:
-            self.status_code = status_code
-        self.payload = payload
-
-    def to_dict(self):
-        rv = dict(self.payload or ())
-        rv['message'] = self.message
-        return rv
-
-
-@app.errorhandler(InvalidUsage)
-def handle_invalid_usage(error):
-    response = jsonify(error.to_dict())
-    response.status_code = error.status_code
-
-    return response
 
 
 if __name__ == '__main__':
