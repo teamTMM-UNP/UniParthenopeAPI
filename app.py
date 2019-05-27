8fd32efa17a3012a7b4a45081d3e403de734a874
g.mell1 <shottyno90@gmail.com>
2019-05-27 22:40:37 +0200

    medie

--- app.py
+++ app.py
@@ -8,8 +8,11 @@ import os
 
 app = Flask(__name__)
 url = "https://uniparthenope.esse3.cineca.it/e3rest/api/"
+
+
 class BaseConfig(object):
     DATA_FOLDER="data_folder"
+
 
 config = {
     "default": BaseConfig
@@ -26,33 +29,73 @@ configure_app(app)
 
 api = Api(app)
 
+
 @api.route('/api/uniparthenope/login/<token>',methods=['GET'])
 class Login(Resource):
-    def get(self,token):
+    def get(self, token):
         headers = {
             'Content-Type': "application/json",
             "Authorization": "Basic " + token
         }
         response = requests.request("GET", url+"login", headers=headers)
         
-        return jsonify({'response' : response.json()})
+        return jsonify({'response': response.json()})
 
 
 @api.route('/api/uniparthenope/totalexams/<token>/<matId>', methods=['GET'])
-class Login(Resource):
-    def get(self, token,matId):
+class TotalExams(Resource):
+    def get(self, token, matId):
         headers = {
             'Content-Type': "application/json",
             "Authorization": "Basic " + token
         }
-        response = requests.request("GET", url + "libretto-service-v1/libretti/"+ matId +"/stats", headers=headers)
+        response = requests.request("GET", url + "libretto-service-v1/libretti/" + matId + "/stats", headers=headers)
         _response = response.json()
-        print('Response = '+ str(_response))
+        print('Response = ' + str(_response))
         totAdSuperate = _response['numAdSuperate'] + _response['numAdFrequentate']
         return jsonify({'totAdSuperate': totAdSuperate,
                         'numAdSuperate': _response['numAdSuperate'],
                         'cfuPar': _response['umPesoSuperato'],
                         'cfuTot': _response['umPesoPiano']})
+
+
+@api.route('/api/uniparthenope/average/<token>/<matId>/<value>', methods=['GET'])
+class TotalExams(Resource):
+    def get(self, token, matId, value):
+        headers = {
+            'Content-Type': "application/json",
+            "Authorization": "Basic " + token
+        }
+        if value != "P" or value != "A":
+            raise InvalidUsage('This view is gone', status_code=410)
+
+        response = requests.request("GET", url + "libretto-service-v1/libretti/" + matId + "/medie", headers=headers)
+
+        _response = response.json()
+
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+
+    return response
 
 
 if __name__ == '__main__':
