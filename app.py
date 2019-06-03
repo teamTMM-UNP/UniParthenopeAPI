0d089f35718325b5fde4810db81c2d661a9d3321
g.mell1 <shottyno90@gmail.com>
2019-06-03 15:12:13 +0200

    orari

--- app.py
+++ app.py
@@ -589,7 +589,74 @@ class Login(Resource):
 FINE AREA RISTORANTI
 '''
 
+'''
+AREA ORARI ga.uniparthenope.it
+'''
+import csv
+import urllib.request
+import io
+import datetime
 
+@api.route('/api/uniparthenope/orari/cercaCorso/<nome_corso>/<nome_prof>/<nome_studio>/<periodo>', methods=['GET'])
+class Login(Resource):
+    def get(self, nome_corso, nome_prof, nome_studio, periodo):
+        end_date = datetime.datetime.now() + datetime.timedelta(days=int(periodo)*365/12)
+
+        url_n = 'http://ga.uniparthenope.it/report.php?from_day=' + str(datetime.datetime.now().day) + \
+                '&from_month=' + str(datetime.datetime.now().month) + \
+                '&from_year=' + str(datetime.datetime.now().year) + \
+                '&to_day=' + str(end_date.day) + \
+                '&to_month=' + str(end_date.month) + \
+                '&to_year=' + str(end_date.year) + \
+                '&areamatch=Centro+Direzionale&roommatch=&typematch%5B%5D=' + nome_studio + \
+                '&namematch=&descrmatch=&creatormatch=&match_private=0&match_confirmed=1&match_referente=&match_unita_interne=&match_ore_unita_interne=&match_unita_vigilanza=&match_ore_unita_vigilanza=&match_unita_pulizie=&match_ore_unita_pulizie=&match_audio_video=&match_catering=&match_Acconto=&match_Saldo=&match_Fattura=&output=0&output_format=1&sortby=s&sumby=d&phase=2&datatable=1'
+        url_open = urllib.request.urlopen(url_n)
+        csvfile = csv.reader(io.StringIO(url_open.read().decode('utf-16')), delimiter=',')
+
+        print(nome_corso)
+        array = []
+        for row in csvfile:
+            index = 0
+            prof = 0
+            for w in row:
+                if (w.find(nome_prof)) != -1:
+                    print(row[0])
+                    prof = index
+                index += 1
+
+            for word in nome_corso:
+                item = {}
+                if row[0].find(word) != -1 and prof != 0:
+                    item.update({'aula': row[2]})
+                    item.update({'inizio': createDate(row[3])})
+                    item.update({'fine': createDate(row[4])})
+                    item.update({'tot': row[5]})
+                    item.update({'docente': row[prof]})
+                    break
+
+            if item:
+                array.append(item)
+        if array:
+            print(array)
+            return jsonify(array)
+
+
+def createDate(data):
+            mesi = ["gennaio", "febbraio", "marzo", "aprile", "maggio", "giugno", "luglio", "agosto", "settembre",
+                    "ottobre", "novembre", "dicembre"]
+            data = data.split()
+            ##print(data)
+            ora = int(data[0][0:2])
+            minuti = int(data[0][3:5])
+            anno = int(data[5])
+            giorno = int(data[3])
+            mese = mesi.index(data[4]) + 1
+
+            final_data = datetime.datetime(anno, mese, giorno, ora, minuti)
+            return final_data
+'''
+FINE AREA ORARI ga.uniparthenope.it
+'''
 def extractData(data):
     data_split = data.split()[0]
     export_data = datetime.strptime(data_split, '%d/%m/%Y')
